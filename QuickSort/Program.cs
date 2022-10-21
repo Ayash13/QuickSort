001b4926f239d173d2fdc3005995d58d011691a9
Muhammad Ayash <muhammad.ayash.ft21@mail.umy.ac.id>
2022-10-21 17:02:00 +0700

    Create Algorithm

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -18,6 +18,11 @@ namespace QUickSort
             {
                 Console.WriteLine("Enter the number of elements in the array");
                 string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("Array can have maximum 20 elements");
             }
         }
     }
