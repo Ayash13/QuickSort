37ab4541031de965a641e9765da7f2fff731c9c7
Muhammad Ayash <muhammad.ayash.ft21@mail.umy.ac.id>
2022-10-21 16:55:55 +0700

    Add project files.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace QUickSort
+{
+    class Program
+    {
+        //array of intergers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparison 
+        private int mov_count = 0; //number of data movements
+
+        //number of elements in array
+        private int n;
+    }
+}
